544b94bfbdd448390636f0801a7334c9e08a7ff8
Alexey-Gorulev <lex-good-go@yandex.ru>
2020-05-17 12:08:36 +0300

    test

--- Folder_1/Script.py
+++ Folder_1/Script.py
@@ -1,7 +1,9 @@
 import os
-import checksumdir
+from checksumdir import dirhash
+
+
 CURRENT_DIR = os.getcwd()
 TARGET_DIR = CURRENT_DIR + "/Folder_1"
 #print(TARGET_DIR)
-hash = checksumdir.dirhash(TARGET_DIR)
+hash = dirhash(TARGET_DIR)
 print(hash)
